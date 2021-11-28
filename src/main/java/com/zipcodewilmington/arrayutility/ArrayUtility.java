fa898af15bbd51a063e979623704bf4c670f39f4
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-28 13:15:25 -0500

    1 down, 3 to go

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,16 +3,56 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
-    public Integer[] removeValue(Integer valueToRemove) {
+public class ArrayUtility <genericType> {
+
+    // declaring an array of some generic type
+    genericType[] anArray;
+
+    public ArrayUtility(genericType[] input) {
+        this.anArray = input;
     }
 
-    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+
+    public genericType[] removeValue(genericType valueToRemove) {
+
+        return null;
     }
 
-    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+    public Integer getNumberOfOccurrences(genericType valueToEvaluate) {
+
+
+        return null;
     }
 
-    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+    public genericType getMostCommonFromMerge(genericType[] arrayToMerge) {
+
+        return null;
     }
+
+    // changed parameter types to generic types, this will cover different types of parameters in our tests
+    public Integer countDuplicatesInMerge(genericType[] arrayToMerge, genericType valueToEvaluate) {
+
+        // made a counter to see how many times valueToEvaluate orrurs in the two arrays
+        int howManyDuplicates = 0;
+
+        // looping through to see how many times valueToEval appears
+        for (genericType element : anArray) {
+            if (element != valueToEvaluate) {
+                continue;
+            }
+            howManyDuplicates++;
+        }
+
+        // looping through to see how many times valueToEval appears
+        for (genericType element : arrayToMerge) {
+            if (element != valueToEvaluate) {
+                continue;
+            }
+            howManyDuplicates++;
+        }
+
+        // returning the final count after looping through both
+        return  howManyDuplicates;
+    }
+
 }
