eb0bc1a0f09c69e882bc6db6ccd1fb94380a573c
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-28 12:45:41 -0500

    beginnings

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -4,4 +4,15 @@ package com.zipcodewilmington.arrayutility;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility {
+    public Integer[] removeValue(Integer valueToRemove) {
+    }
+
+    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+    }
+
+    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+    }
+
+    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+    }
 }
